88879f27010900ac02d12f97b48eb15b9f28db28
mariamajankeh <jankehsecka22@gmail.com>
2022-09-02 09:42:15 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const middleware = require('./utilities/middleware')
 
  
 
@@ -10,11 +11,75 @@ app.get('/', (req, res) => {
   res.send('livescore for the Gambian football league')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
 app.get('/me',(req, res) => {
     req.send('this is me')
 })
     
+    
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+app.get('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  const user = users.find((user) => user.id ===Number (id))
+  res.json(user)
+  })
+
+  app.get('/api/users', (req, res) => {
+    res.json(users)
+  })
+
+  app.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    users = users.filter((u) => u.id !== Number(id))
+
+    res.status(204).end()
+  })
+  
+
+  console.log(`Example app listening on port ${port}`)
+
+app.post('api/users/:id', (req, res) => {
+   const content = req.body
+
+   res.json(content)
+
+})
+
+app.put('/api/users/:id', (req, res) =>{
+  const id = req.params.id
+
+})
+
+app.use(middleware.unknownEndpoint)
+
+
+
+
+
+
+  
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
